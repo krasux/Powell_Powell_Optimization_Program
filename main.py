01b6efe0fae63e8bb28ddc7d33feb0f20ff20da2
sylwester <sylwester.kubicki@gmail.com>
2015-05-16 22:37:56 +0200

    Rearrange ui, add delta and theta parameters. Parameters for Powell can be now changed.

--- main.py
+++ main.py
@@ -12,9 +12,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     numberOfVariables = 2
     meritum = 0
     constraints = []
+    deltas = []
+    thetas = []
     xStart = 0
     maxIterations = 0
     epsilonPowell = 10e-6
+    m1 = 0.250
+    m2 = 10
+    cMin = 0.2
+
     def __init__(self):
         super(MainWindow, self).__init__()
         QToolTip.setFont(QFont('SansSerif', 10))
@@ -22,10 +28,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # Make some local modifications.
-        self.btnSearch.setToolTip("Click to start the program")
+        self.btnSearch.setToolTip("Click to start the program \u03B4 \u03B8")
         self.cmbVariables.setCurrentIndex(1)
-        self.cmbPowellEpsilon.setCurrentIndex(4)
+        self.cmbPowellEpsilon.setCurrentIndex(3)
 
+        # Set lbl for sigma (\u03B4) and theta (\u03B8)
+        self.lblConstrDelta1.setText("\u03B41")
+        self.lblConstrDelta2.setText("\u03B42")
+        self.lblConstrDelta3.setText("\u03B43")
+        self.lblConstrDelta4.setText("\u03B44")
+        self.lblConstrDelta5.setText("\u03B45")
+
+        self.lblConstrTheta1.setText("\u03B81")
+        self.lblConstrTheta2.setText("\u03B82")
+        self.lblConstrTheta3.setText("\u03B83")
+        self.lblConstrTheta4.setText("\u03B84")
+        self.lblConstrTheta5.setText("\u03B85")
 
         # Connect up the buttons and combo boxes.
         self.cmbConstraints.activated.connect(self.nbConstrChanged)
@@ -45,12 +63,33 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.inputX0_4.editingFinished.connect(self.x0Changed)
         self.inputX0_5.editingFinished.connect(self.x0Changed)
 
-        # Connect constraints inputs:
+        # Connect constraints equations inputs:
         self.inputConstraint1.editingFinished.connect(self.constraintsChanged)
         self.inputConstraint2.editingFinished.connect(self.constraintsChanged)
         self.inputConstraint3.editingFinished.connect(self.constraintsChanged)
         self.inputConstraint4.editingFinished.connect(self.constraintsChanged)
         self.inputConstraint5.editingFinished.connect(self.constraintsChanged)
+
+        # Connect constraints parameters:
+        self.dsbConstrDelta1.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrDelta2.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrDelta3.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrDelta4.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrDelta5.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrTheta1.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrTheta2.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrTheta3.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrTheta4.valueChanged.connect(self.constraintsChanged)
+        self.dsbConstrTheta5.valueChanged.connect(self.constraintsChanged)
+
+
+        # Connect Powell parameters:
+        self.dSBM1.valueChanged.connect(self.m1ChangedDSBox)
+        self.hSldM1.valueChanged.connect(self.m1ChangedHSld)
+        self.dSBM2.valueChanged.connect(self.m2ChangedDSBox)
+        self.hSldM2.valueChanged.connect(self.m2ChangedHSld)
+        self.dSBcMin.valueChanged.connect(self.cMinChangedDSBox)
+        self.hSldcMin.valueChanged.connect(self.cMinChangedHSld)
 
 
         # Set up variables and constrains inputs:
@@ -63,10 +102,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         # Show the window at the end
         self.show()
 
+    # Methods for constraints
     def nbConstrChanged(self):
         self.numberOfConstraints = int((self.cmbConstraints.currentText()))
         self.setActiveConstrainInputs(self.numberOfConstraints)
-        self.setConstraints(self.numberOfConstraints)
+        self.setConstrEquations(self.numberOfConstraints)
+        self.setConstrParameters(self.numberOfConstraints)
 
     def setActiveConstrainInputs(self, nbrOfConstr):
         self.inputConstraint1.setDisabled(True)
@@ -79,29 +120,126 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.lblConstraint3.setDisabled(True)
         self.lblConstraint4.setDisabled(True)
         self.lblConstraint5.setDisabled(True)
+        self.lblConstraint1_2.setDisabled(True)
+        self.lblConstraint2_2.setDisabled(True)
+        self.lblConstraint3_2.setDisabled(True)
+        self.lblConstraint4_2.setDisabled(True)
+        self.lblConstraint5_2.setDisabled(True)
+        self.lblConstrEq1.setDisabled(True)
+        self.lblConstrEq2.setDisabled(True)
+        self.lblConstrEq3.setDisabled(True)
+        self.lblConstrEq4.setDisabled(True)
+        self.lblConstrEq5.setDisabled(True)
+        self.lblConstrDelta1.setDisabled(True)
+        self.lblConstrDelta2.setDisabled(True)
+        self.lblConstrDelta3.setDisabled(True)
+        self.lblConstrDelta4.setDisabled(True)
+        self.lblConstrDelta5.setDisabled(True)
+        self.lblConstrTheta1.setDisabled(True)
+        self.lblConstrTheta2.setDisabled(True)
+        self.lblConstrTheta3.setDisabled(True)
+        self.lblConstrTheta4.setDisabled(True)
+        self.lblConstrTheta5.setDisabled(True)
+        self.dsbConstrDelta1.setDisabled(True)
+        self.dsbConstrDelta2.setDisabled(True)
+        self.dsbConstrDelta3.setDisabled(True)
+        self.dsbConstrDelta4.setDisabled(True)
+        self.dsbConstrDelta5.setDisabled(True)
+        self.dsbConstrTheta1.setDisabled(True)
+        self.dsbConstrTheta2.setDisabled(True)
+        self.dsbConstrTheta3.setDisabled(True)
+        self.dsbConstrTheta4.setDisabled(True)
+        self.dsbConstrTheta5.setDisabled(True)
+
+
 
         if nbrOfConstr >= 1:
             self.inputConstraint1.setDisabled(False)
             self.lblConstraint1.setDisabled(False)
+            self.lblConstraint1_2.setDisabled(False)
+            self.lblConstrEq1.setDisabled(False)
+            self.lblConstrDelta1.setDisabled(False)
+            self.lblConstrTheta1.setDisabled(False)
+            self.dsbConstrDelta1.setDisabled(False)
+            self.dsbConstrTheta1.setDisabled(False)
         if nbrOfConstr >= 2:
             self.inputConstraint2.setDisabled(False)
             self.lblConstraint2.setDisabled(False)
+            self.lblConstraint2_2.setDisabled(False)
+            self.lblConstrEq2.setDisabled(False)
+            self.lblConstrDelta2.setDisabled(False)
+            self.lblConstrTheta2.setDisabled(False)
+            self.dsbConstrDelta2.setDisabled(False)
+            self.dsbConstrTheta2.setDisabled(False)
         if nbrOfConstr >= 3:
             self.inputConstraint3.setDisabled(False)
             self.lblConstraint3.setDisabled(False)
+            self.lblConstraint3_2.setDisabled(False)
+            self.lblConstrEq3.setDisabled(False)
+            self.lblConstrDelta3.setDisabled(False)
+            self.lblConstrTheta3.setDisabled(False)
+            self.dsbConstrDelta3.setDisabled(False)
+            self.dsbConstrTheta3.setDisabled(False)
         if nbrOfConstr >= 4:
             self.inputConstraint4.setDisabled(False)
             self.lblConstraint4.setDisabled(False)
+            self.lblConstraint4_2.setDisabled(False)
+            self.lblConstrEq4.setDisabled(False)
+            self.lblConstrDelta4.setDisabled(False)
+            self.lblConstrTheta4.setDisabled(False)
+            self.dsbConstrDelta4.setDisabled(False)
+            self.dsbConstrTheta4.setDisabled(False)
         if nbrOfConstr >= 5:
             self.inputConstraint5.setDisabled(False)
             self.lblConstraint5.setDisabled(False)
+            self.lblConstraint5_2.setDisabled(False)
+            self.lblConstrEq5.setDisabled(False)
+            self.lblConstrDelta5.setDisabled(False)
+            self.lblConstrTheta5.setDisabled(False)
+            self.dsbConstrDelta5.setDisabled(False)
+            self.dsbConstrTheta5.setDisabled(False)
 
+    def constraintsChanged(self):
+        self.setConstrEquations(self.numberOfConstraints)
+        self.setConstrParameters(self.numberOfConstraints)
 
+    def setConstrEquations(self, nbrOfConstr):
+        self.constraints = []
+        if nbrOfConstr >= 1:
+            self.constraints.append(str2fun(self.inputConstraint1.text()))
+        if nbrOfConstr >= 2:
+            self.constraints.append(str2fun(self.inputConstraint2.text()))
+        if nbrOfConstr >= 3:
+            self.constraints.append(str2fun(self.inputConstraint3.text()))
+        if nbrOfConstr >= 4:
+            self.constraints.append(str2fun(self.inputConstraint4.text()))
+        if nbrOfConstr >= 5:
+            self.constraints.append(str2fun(self.inputConstraint5.text()))
+
+    def setConstrParameters(self, nbrOfConstr):
+        self.deltas = []
+        self.thetas = []
+        if nbrOfConstr >= 1:
+            self.deltas.append(self.dsbConstrDelta1.value())
+            self.thetas.append(self.dsbConstrTheta1.value())
+        if nbrOfConstr >= 2:
+            self.deltas.append(self.dsbConstrDelta2.value())
+            self.thetas.append(self.dsbConstrTheta2.value())
+        if nbrOfConstr >= 3:
+            self.deltas.append(self.dsbConstrDelta3.value())
+            self.thetas.append(self.dsbConstrTheta3.value())
+        if nbrOfConstr >= 4:
+            self.deltas.append(self.dsbConstrDelta4.value())
+            self.thetas.append(self.dsbConstrTheta4.value())
+        if nbrOfConstr >= 5:
+            self.deltas.append(self.dsbConstrDelta5.value())
+            self.thetas.append(self.dsbConstrTheta5.value())
+
+    # Methods for variables
     def nbVarChanged(self):
         self.numberOfVariables = int((self.cmbVariables.currentText()))
         self.setActiveVariableInputs(self.numberOfVariables)
         self.setXStart(self.numberOfVariables)
-
 
     def setActiveVariableInputs(self, nbrOfVar):
         self.inputX0_2.setDisabled(True)
@@ -125,8 +263,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         if nbrOfVar >= 5:
             self.inputX0_5.setDisabled(False)
             self.lbX0_5.setDisabled(False)
-
-
 
     def setMeritum(self):
         self.meritum = str2fun(self.cmbObjFun.currentText())
@@ -163,22 +299,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def setPowellEpsilon(self, newEpsilon):
         self.epsilonPowell = newEpsilon
 
-    def constraintsChanged(self):
-        self.setConstraints(self.numberOfConstraints)
-
-    def setConstraints(self, nbrOfConstr):
-        self.constraints = []
-        if nbrOfConstr >= 1:
-            self.constraints.append(str2fun(self.inputConstraint1.text()))
-        if nbrOfConstr >= 2:
-            self.constraints.append(str2fun(self.inputConstraint2.text()))
-        if nbrOfConstr >= 3:
-            self.constraints.append(str2fun(self.inputConstraint3.text()))
-        if nbrOfConstr >= 4:
-            self.constraints.append(str2fun(self.inputConstraint4.text()))
-        if nbrOfConstr >= 5:
-            self.constraints.append(str2fun(self.inputConstraint5.text()))
-
     def startSearch(self):
         self.statusBar().showMessage("Searching for minimum of: " + str(self.cmbObjFun.currentText())
                                      + " With xStart: " + str(self.xStart))
@@ -188,7 +308,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.logResults.insertPlainText("Starting x: " + str(self.xStart) + '\n')
 
         xMin, nIter, success = powell(self.meritum, self.xStart, epsilon=self.epsilonPowell,
-                                      iterations=self.maxIterations, searchRanges=self.xSearchRanges)
+                                      iterations=self.maxIterations)
         if success:
             self.logResults.insertPlainText('Minimum found at x: ' + str(xMin) + '\n')
         else:
@@ -199,7 +319,41 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         sb = self.logResults.verticalScrollBar()
         sb.setValue(sb.maximum())
 
+    # Set and get parameter M1
+    def setM1(self, newM1):
+        self.m1 = newM1
+        self.hSldM1.setValue(newM1*1000)
+        self.dSBM1.setValue(newM1)
 
+    def m1ChangedDSBox(self):
+        self.setM1(self.dSBM1.value())
+
+    def m1ChangedHSld(self):
+        self.setM1(self.hSldM1.value()/1000)
+
+    # Set and get parameter M2
+    def setM2(self, newM2):
+        self.m2 = newM2
+        self.hSldM2.setValue(newM2*10)
+        self.dSBM2.setValue(newM2)
+
+    def m2ChangedDSBox(self):
+        self.setM2(self.dSBM2.value())
+
+    def m2ChangedHSld(self):
+        self.setM2(self.hSldM2.value()/10)
+
+    # Set and get parameter cMin
+    def setcMin(self, newcMin):
+        self.cMin = newcMin
+        self.hSldcMin.setValue(newcMin*1000)
+        self.dSBcMin.setValue(newcMin)
+
+    def cMinChangedDSBox(self):
+        self.setcMin(self.dSBcMin.value())
+
+    def cMinChangedHSld(self):
+        self.setcMin(self.hSldcMin.value()/1000)
 
 
 
